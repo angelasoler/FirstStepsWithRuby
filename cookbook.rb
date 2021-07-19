7344ae96aaa76ce9d3f6f55c3047bea032f32f5a
angelasoler <manzanaembicicleta@gmail.com>
2021-07-19 18:11:34 -0300

    corregindo indentação

--- cookbook.rb
+++ cookbook.rb
@@ -1,34 +1,34 @@
 def bem_vindo()
-    puts "Bemvindo ao Cookbook, sua rede social de receitas"
+  puts "Bemvindo ao Cookbook, sua rede social de receitas"
 end
 
 def menu_opcoes()
-    puts "[1] Cadastrar uma receita"
-    puts "[2] Ver todas as receitas"
-    puts "[3] sair"
-
-    print "Escolha uma opção: "
-    return gets.to_i()
+  puts "[1] Cadastrar uma receita"
+  puts "[2] Ver todas as receitas"
+  puts "[3] sair"
+ 
+  print "Escolha uma opção: "
+  return gets.to_i()
 end
 
-def inserir_receita()
-    puts "Digite o nome da receita: "
-    nome = gets.chomp()
-    puts "Digite o tipo da receita: "
-    tipo = gets.chomp()
+def inserir_receita() 
+  puts "Digite o nome da receita: "
+  nome = gets.chomp()
+  puts "Digite o tipo da receita: "
+  tipo = gets.chomp()
 
-    puts
-    puts "Receita #{nome} cadastrada com sucesso!"
-    puts
-    return { nome: nome, tipo: tipo }
+  puts
+  puts "Receita #{nome} cadastrada com sucesso!"
+  puts
+  return { nome: nome, tipo: tipo }
 end
 
 def imprimir_receitas(r)
-    puts"========== Receitas cadastradas =========="
-    r.each do |receita|
-        puts"#{receita[:nome]} - #{receita[:tipo]}"
-    end
-    puts
+  puts"========== Receitas cadastradas =========="
+  r.each do |receita|
+      puts"#{receita[:nome]} - #{receita[:tipo]}"
+  end
+  puts
 end
 
 bem_vindo()
@@ -38,16 +38,16 @@ receitas = []
 opcao = menu_opcoes()
 
 while(opcao != 3) do
-    if(opcao == 1)
-        receitas << inserir_receita()
-    elsif(opcao == 2)
-        imprimir_receitas(receitas)
-        receitas
-    else
-        puts"Opção inválida"
-    end
+  if(opcao == 1)
+    receitas << inserir_receita()
+  elsif(opcao == 2)
+    imprimir_receitas(receitas)
+    receitas
+  else
+    puts"Opção inválida"
+  end
 
-    opcao = menu_opcoes()
+  opcao = menu_opcoes()
 
 end
 
