89079ee515fa06cfb3e29b404add13038df18540
angelasoler <manzanaembicicleta@gmail.com>
2021-07-20 16:30:52 -0300

    classes, atributos e objetos

--- OO/aluno.rb
+++ OO/aluno.rb
@@ -1,34 +1,9 @@
 class Aluno
+  attr_accessor :nome, :telefone, :matricula
+  
   def initialize(nome, telefone, matricula)
     @nome = nome
     @telefone = telefone
     @matricula = matricula
-  end
-
-# getter
-  def telefone
-    return @telefone
-  end
-
-#setter
-  #em ruby podemso delcarar metodos com =
-  def telefone=(valor)
-    @telefone = valor
-  end
-
-  def nome
-    return @nome
-  end
-
-  def nome=(valor)
-    @nome = valor
-  end
-
-  def matricula
-    return @matricula
-  end
-
-  def matricula=(valor)
-    @matricula = valor
   end
 end
--- OO/turma.rb
+++ OO/turma.rb
@@ -1,6 +1,18 @@
-class
-  Truma
+class Turma
+  attr_accessor :nome, :numero_da_sala, :professor, :alunos
 
-  attr_accessor
+  def initialize(nome, numero_da_sala, professor)
+    @nome = nome
+    @numero_da_sala = numero_da_sala
+    @professor = professor
+    @alunos = []
+  end
 
+  def adiciona_aluno(aluno)
+    alunos << aluno
+  end
 
+  def total_alunos()
+    @alunos.size()
+  end
+end
